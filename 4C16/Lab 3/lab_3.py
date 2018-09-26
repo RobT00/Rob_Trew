97baa1598bceb98c077bc8182d6b9862111248d4
Trew <trewr@AD.MEE.TCD.IE>
2018-09-26 11:12:11 +0100

    Updated Notebooks

--- 4C16/Lab 3/lab_3.py
+++ 4C16/Lab 3/lab_3.py
@@ -4,10 +4,12 @@ def question_1():
     # This function should return a string.
     # Uncomment the correct answer.
     #
+    # Quiz: Which classifier has the highest TPR for an FPR of 15%?
+    #
     # Yes, you could try them each in turn..!
     # But don't do that.
-    return ""   # Placeholder
-    # return "Nearest Neighbors"
+    # return ""   # Placeholder
+    return "Nearest Neighbors"
     # return "Logistic Regression"
     # return "Linear SVM"
     # return "RBF SVM"
@@ -16,14 +18,24 @@ def question_1():
 def question_2():
     # This function returns a single number,
     # corresponding to the answer.
-    return -1 # Replace with your answer.
-
+    #
+    # Quiz: Identify the numerical value of the weight which minimizes the fpr for total recall
+    #
+    # return -1 # Replace with your answer.
+    return 1.35
+    
 def question_3():
     # This function returns a single number,
     # corresponding to the answer.
-    return -1 # Replace with your answer.
+    #
+    # What percentage of images in the test set are misclassified by the RBF SVM classifier?
+    #
+    return 0.062 # Replace with your answer.
 
 def question_4():
     # This function returns a single number, corresponding to the
     # digit most commonly misclassified.
-    return -1 # Replace with your answer.
+    #
+    # Which digit is most frequently misclassified?
+    #
+    return 7 # Replace with your answer.
